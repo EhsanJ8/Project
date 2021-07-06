1b9f94bf3b5cbbdf0c20799904b49bdd02923254
Amirreza-Gh-19 <amirreza20144@yahoo.com>
2021-07-06 19:55:50 +0430

    code e avalieh

--- first.py
+++ first.py
@@ -1,1 +1,16 @@
-#salam
+import requests
+import json
+
+url="https://api.telegram.org/bot1848540350:AAG5JclNyx3863mB19cvYGlx5K8Adlcvge4/"
+
+def get_all_updates():
+    reponse=requests.get(url + "getUpdates")
+    return reponse.json()
+
+def get_last_update(allupdates):
+    return allupdates["result"][-1]
+
+
+data=get_all_updates()
+lastupdate=get_last_update(data)
+print()
